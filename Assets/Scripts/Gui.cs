8031d82b39264f10bad8fe5a672483e43ff3777c
Kerey Roper <kerey@rivetinggames.com>
2013-12-18 11:34:22 -0800

    adding stub scaling scripts

---
+++ Assets/Scripts/Gui.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gui : MonoBehaviour
+{
+	public ScaleTester scaleTester;
+
+	void OnGUI () {
+		if (GUILayout.Button("Scale Up")) {
+			scaleTester.ScaleUp();
+		}
+		if (GUILayout.Button("Scale Down")) {
+			scaleTester.ScaleDown();
+		}
+	}
+}
---
+++ Assets/Scripts/ScaleTester.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleTester : MonoBehaviour
+{
+	public GameObject dynamicObjectRoot;
+
+	private int scaleLevel;
+
+	public void ScaleDown() {
+		scaleLevel--;
+	}
+
+	public void ScaleUp() {
+		scaleLevel++;
+	}
+
+}
